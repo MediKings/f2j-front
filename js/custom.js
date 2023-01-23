314150b7fbe370040ed7c7b74d2917503f58f7b5
Medi King's <jehovahmbote3@gmail.com>
2023-01-23 16:11:07 +0100

    Command form

--- js/custom.js
+++ js/custom.js
@@ -7,32 +7,31 @@ function getYear() {
 
 getYear();
 
-
 // client section owl carousel
-$(".client_owl-carousel").owlCarousel({
-    loop: true,
-    margin: 0,
-    dots: false,
-    nav: true,
-    navText: [],
-    autoplay: true,
-    autoplayHoverPause: true,
-    navText: [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    responsive: {
-        0: {
-            items: 1
-        },
-        768: {
-            items: 2
-        },
-        1000: {
-            items: 2
-        }
-    }
-});
+// $(".client_owl-carousel").owlCarousel({
+//     loop: true,
+//     margin: 0,
+//     dots: false,
+//     nav: true,
+//     navText: [],
+//     autoplay: true,
+//     autoplayHoverPause: true,
+//     navText: [
+//         '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+//         '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+//     ],
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         768: {
+//             items: 2
+//         },
+//         1000: {
+//             items: 2
+//         }
+//     }
+// });
 
 
 
@@ -46,11 +45,10 @@ $('#products1').slick({
  })
 
 
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+// Command form
+$('#open-command-form').click(()=>{
+    $('#command-form').toggleClass('d-none d-flex');
+})
+$('#close-command-form').click(()=>{
+    $('#command-form').toggleClass('d-none d-flex');
+})
